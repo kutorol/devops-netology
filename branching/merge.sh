475e1afcf6710f7d88ce8352006cacf64059ba8b
Дмитриев Николай <nikolay.d.dmitriev@lenta.com>
2022-03-05 12:50:32 +0300

    merge: @ instead *

--- branching/merge.sh
+++ branching/merge.sh
@@ -4,9 +4,9 @@
 count=1
 count2=1
 # shellcheck disable=SC2066
-for param in "$*"; do
+for param in "$@"; do
     echo "$count - $count2"
-    echo "\$* Parameter #$count = $param"
+    echo "\$@ Parameter #$count = $param"
     count=$(( $count + 1 ))
     # shellcheck disable=SC2004
     count2=$(( $count2 + 1 ))
